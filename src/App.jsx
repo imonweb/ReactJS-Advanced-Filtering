571db4f04e94d9c6bc041a0cc8e079d1ec6ed95f
Imon <contact@imonweb.co.uk>
2023-12-12 13:51:06 +0000

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+ 
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>React JS Advanced Filtering</h1>
+      </div>  
+    </>
+  )
+}
+
+export default App
