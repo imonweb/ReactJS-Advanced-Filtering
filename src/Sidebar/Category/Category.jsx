0413a8c1c6b3154bbb32ee32870b0add36a78fef
Imon <contact@imonweb.co.uk>
2023-12-15 01:47:54 +0000

    refactor

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -2,7 +2,56 @@ import './Category.scss'
 
 const Category = () => {
   return (
-    <div>Category</div>
+    <div className='category'>
+      <h2 className="sidebar-title">Category</h2>
+
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" value=""/>
+          <span className="checkmark"></span> All
+        </label>
+
+        <label className="sidebar-label-container">
+          <input 
+            type="radio" 
+            name="test" 
+            value="sneakers"
+            title="Sneakers"  
+          />
+          <span className="checkmark"></span> Sneakers
+        </label>
+
+        <label className="sidebar-label-container">
+          <input 
+            type="radio" 
+            name="test" 
+            value="flats"
+            title="Flats"
+          />
+          <span className="checkmark"></span> Flats
+        </label>
+
+        <label className="sidebar-label-container">
+          <input 
+            type="radio" 
+            name="test" 
+            value="sandals"
+            title="Sandals"
+          />
+          <span className="checkmark"></span> Sandals
+        </label>
+
+        <label className="sidebar-label-container">
+          <input 
+            type="radio" 
+            name="test" 
+            value="heels"
+            title="Heels"
+          />
+          <span className="checkmark"></span> Heels
+        </label>
+      </div>
+    </div>
   )
 }
 
--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -2,7 +2,38 @@ import './Colors.scss'
 
 const Colors = () => {
   return (
-    <div>Colors</div>
+    <div>
+       <label className="sidebar-label-container color-title">
+          <input type="radio" name="test2" value=""/>
+          <span className="checkmark"></span> All
+        </label>
+
+         <label className="sidebar-label-container">
+          <input type="radio" name="test2" value=""/>
+          <span className="checkmark"></span> Black
+        </label>
+
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" value=""/>
+          <span className="checkmark"></span> Blue
+        </label>
+
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" value=""/>
+          <span className="checkmark"></span> Red
+        </label>
+
+        <label className="sidebar-label-container">
+          <input type="radio" name="test2" value=""/>
+          <span className="checkmark"></span> Green
+        </label>
+
+         <label className="sidebar-label-container">
+          <input type="radio" name="test2" value=""/>
+          <span className="checkmark"></span> White
+        </label>
+
+    </div>
   )
 }
 
