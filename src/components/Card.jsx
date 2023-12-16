7d9e88dd71c35798dc18838efd54477630517e84
Imon <contact@imonweb.co.uk>
2023-12-16 23:48:22 +0000

    refactor

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,14 @@
-import { AiFillStar } from 'react-icons/ai'
+// import { AiFillStar } from 'react-icons/ai'
 import { BsFillBagHeartFill } from 'react-icons/bs'
 
-const Card = () => {
+const Card = ({ img, title, star, reviews, prevPrice, newPrice }) => {
   return (
      <section className="card__container--card">
         <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt="Shoe" className='card__container--card-img' />
         <div className="card__details">
           <h3 className="card__details__card-title">Shoe</h3>
           <section className="card__details__card-reviews">
-            <AiFillStar className='ratings-star' />
-            <AiFillStar className='ratings-star' />
-            <AiFillStar className='ratings-star' />
-            <AiFillStar className='ratings-star' />
+            {star}
             <span className="card__details__card-reviews--total-reviews">4</span>
           </section>
           <section className="card__details__card_price">
